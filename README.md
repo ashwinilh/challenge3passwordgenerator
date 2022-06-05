841cecd6394e04b13c2b13a01c204665af231d4d
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-06-04 23:43:45 -0500

    variables declaration

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 This is the repo for random password generator
 1. copied starter code.
 2. updated href for both style sheets and javascript.
+3. declared variables.
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,11 @@
-// Assignment code here
+// varaiables declaration
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharacterArr = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', ' =', '<', '>', '?', '|'];
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 
 // Get references to the #generate element
