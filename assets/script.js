1dbf28d51eb15df43a959fcefdfb2e5e8b4d5111
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-06-04 23:48:58 -0500

    created prompts for the user

--- assets/script.js
+++ assets/script.js
@@ -22,3 +22,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function getPrompts() {
+    choiceArr = [];
+    characterLength = parseInt(prompt("How many characters do you want your password to be? (8-128 characters)"));
+    if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+      alert("character length has to be a number, 8-128 digits. Please try again.")
+      return false;
+    }
+    if (confirm("Would you like lowercase letters in your password?")) {
+      choiceArr = choiceArr.concat(lowerCaseArr);
+    }
+    if (confirm("Would you like uppercase letters in your password?")) {
+      choiceArr = choiceArr.concat(upperCaseArr);
+    }
+    if (confirm("Would you like special character letters in your password?")) {
+      choiceArr = choiceArr.concat(specialCharacterArr);
+    }
+    if (confirm("Would you like numbers letters in your password?")) {
+      choiceArr = choiceArr.concat(numberArr);
+    }
+    return true;
+  }
